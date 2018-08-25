77ac69f1bd42f51de7d2a4bd8511a42ef848160f
belijzajac <tautvydas749@gmail.com>
2018-08-25 22:15:15 +0300

    small UI fixes

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;    
+    MainWindow w;
+    w.setWindowFlags(Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint);
     w.show();
 
     return a.exec();
