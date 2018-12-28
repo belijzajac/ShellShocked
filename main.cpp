ba829533e3dcb9e2128285a8a0f3bb79bf0693ac
belijzajac <tautvydas749@gmail.com>
2018-12-28 20:26:27 +0200

    Refactored code

--- main.cpp
+++ main.cpp
@@ -5,7 +5,13 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowFlags(Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint);
+    w.setWindowFlag(Qt::WindowStaysOnTopHint);
+
+    // run ./ShellShocked --frameless to have a frameless window
+    // on KDE this is a recommended option
+    if(argc == 2 && strcmp(argv[1], "--frameless") == 0)
+        w.setWindowFlags(Qt::FramelessWindowHint);
+
     w.show();
 
     return a.exec();
